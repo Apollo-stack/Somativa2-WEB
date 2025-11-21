1c4023bee8dc83c8a53399dc3250dba4cd177b71
Ramon <Matheusramon0208@gmail.com>
2025-11-21 15:15:06 -0300

    Container para alinhar os itens e link para o style

--- processa_cadastro.php
+++ processa_cadastro.php
@@ -7,33 +7,40 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet"> 
+
 </head>
 <body>
+
+    <div class="container d-flex justify-content-center align-items-center vh-100">
+        <div class="card shadow-sm p-4 text-center">
     
-    <?php 
-    
-    require_once 'conexao.php';
-    
-        $nome = $_POST["nome"];
-        $email = $_POST["email"];
-        $senha = $_POST["senha"];
+            <?php 
+            
+            
+            require_once 'conexao.php';
+            
+                $nome = $_POST["nome"];
+                $email = $_POST["email"];
+                $senha = $_POST["senha"];
 
-        $senha_hash = password_hash($senha, PASSWORD_DEFAULT);
+                $senha_hash = password_hash($senha, PASSWORD_DEFAULT);
 
-        $sql = "INSERT INTO usuarios (nome_usuario, email, senha) VALUES (?, ?, ?)"; 
-        $stmt = $conn->prepare($sql);
+                $sql = "INSERT INTO usuarios (nome_usuario, email, senha) VALUES (?, ?, ?)"; 
+                $stmt = $conn->prepare($sql);
 
-        $stmt->bind_param("sss", $nome, $email, $senha_hash);
+                $stmt->bind_param("sss", $nome, $email, $senha_hash);
 
-        if ($stmt->execute()) {
-            echo "Usuário cadastrado com sucesso!";
-        } else {
-            echo "Erro ao cadastrar: " . $stmt->error;
-        }
+                if ($stmt->execute()) {
+                    echo "Usuário cadastrado com sucesso!";
+                } else {
+                    echo "Erro ao cadastrar: " . $stmt->error;
+                }
 
-        $stmt->close();
-        $conn->close();
-    ?>
-    
+                $stmt->close();
+                $conn->close();
+            ?>
+        </div>
+    </div>
 </body>
 </html>
