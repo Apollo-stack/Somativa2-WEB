a8cdc4b1b144e7de838e9676b868ff4091dd14ef
Ramon <Matheusramon0208@gmail.com>
2025-11-27 15:41:46 -0300

    Corrigir caminhos de redirecionamento e remover imagens não utilizadas

--- Excluir/excluir_autor.php
+++ Excluir/excluir_autor.php
@@ -4,12 +4,12 @@ session_start();
 
 
 if (!isset($_SESSION['usuario_id'])){
-    header("Location: index.html"); 
+    header("Location: ../index.html"); 
     exit; 
 }
 
 
-require_once 'conexao.php';
+require_once '../conexao.php';
 
 
 $id_autor = $_GET['id'];
@@ -26,6 +26,6 @@ $stmt->close();
 $conn->close();
 
 
-header("Location: dashboard.php");
+header("Location: ../dashboard.php");
 exit;
 ?>
