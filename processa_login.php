a3e0c5bb1b407d24fc66328bb873c7b92ca48f60
Ramon <Matheusramon0208@gmail.com>
2025-11-27 15:13:20 -0300

    Atualizar redirecionamentos para a página inicial após falhas de login e cadastro

--- processa_login.php
+++ processa_login.php
@@ -28,7 +28,7 @@ if ($resultado->num_rows === 1) {
 
     } else {
         
-        header("Location: login.html?erro=true");
+        header("Location: index.html?erro=true");
         exit;
     }
     
