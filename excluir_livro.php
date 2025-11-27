b8994c0c5eae9420d25597165eab796d5f5e9e0d
Ramon <Matheusramon0208@gmail.com>
2025-11-27 10:12:27 -0300

    Removed unnecessary blank lines in excluir_livro.php for cleaner code.

--- excluir_livro.php
+++ excluir_livro.php
@@ -1,7 +1,5 @@
 <?php 
-
 session_start();
-
 
 if (!isset($_SESSION['usuario_id'])){
     header("Location: index.html"); 
@@ -13,13 +11,10 @@ require_once 'conexao.php';
 
 
 $id_livro = $_GET['id'];
-
-
 $sql = "DELETE FROM livros WHERE id_livro = ?";
 
 
 $stmt = $conn->prepare($sql); 
-
 
 $stmt->bind_param("i", $id_livro);
 
@@ -29,7 +24,6 @@ $stmt->execute();
 
 $stmt->close();
 $conn->close();
-
 
 header("Location: dashboard.php");
 exit;
