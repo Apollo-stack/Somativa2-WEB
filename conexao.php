5a1529cca984dd3eadca2e767c96b24ed541bf76
Ramon <Matheusramon0208@gmail.com>
2025-11-29 08:42:53 -0300

    nada mudou

--- conexao.php
+++ conexao.php
@@ -4,9 +4,8 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "Biblioteca";
-$port = 3307;
 
-$conn = new mysqli($servername, $username, $password, $dbname, $port);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 
 if(!$conn){
